065475a8c38ff15768eb6c680370d401cef983d9
Analia Serafini <analiaserafiniv@gamil.com>
2025-04-22 22:38:47 -0300

    imagenes en menos calidad y

---
+++ backup/cache/com_contact/fc52438e3606ca2a7a6ab105e2b38b92.php
@@ -0,0 +1,162 @@
+<?php die("Access Denied"); ?>
+a:4:{s:4:"body";s:3881:"<div id="component-contact">
+<table width="100%" cellpadding="0" cellspacing="0" border="0" class="contentpaneopen">
+<tr>
+	<td width="100%" class="contentheading">
+		ASyA Contacto	</td>
+</tr>
+<tr>
+	<td>
+		<table border="0" width="100%">
+		<tr>
+			<td></td>
+			<td rowspan="2" align="right" valign="top">
+						</td>
+		</tr>
+		<tr>
+			<td>
+				<table width="100%" cellpadding="0" cellspacing="0" border="0">
+<tr>
+	<td rowspan="6" valign="top" width="40" >
+		<img src="/images/M_images/con_address.png" alt="Dirección: "  />	</td>
+</tr>
+<tr>
+	<td valign="top">
+		Lavalle 1546 6to D	</td>
+</tr>
+<tr>
+	<td valign="top">
+		Capital Federal	</td>
+</tr>
+<tr>
+	<td valign="top">
+		Buenos Aires	</td>
+</tr>
+<tr>
+	<td valign="top">
+		C1048AAL	</td>
+</tr>
+<tr>
+	<td valign="top">
+		Argentina	</td>
+</tr>
+</table>
+<br />
+<table width="100%" cellpadding="0" cellspacing="0" border="0">
+<tr>
+	<td width="40" >
+		<img src="/templates/youmedia/images/emailButton.png" alt="E-mail: "  />	</td>
+	<td>
+		
+ <script language='JavaScript' type='text/javascript'>
+ <!--
+ var prefix = 'm&#97;&#105;lt&#111;:';
+ var suffix = '';
+ var attribs = '';
+ var path = 'hr' + 'ef' + '=';
+ var addy59958 = '&#97;sy&#97;' + '&#64;';
+ addy59958 = addy59958 + '&#97;sy&#97;c&#111;ns&#117;lt&#111;r&#101;s' + '&#46;' + 'c&#111;m' + '&#46;' + '&#97;r';
+ document.write( '<a ' + path + '\'' + prefix + addy59958 + suffix + '\'' + attribs + '>' );
+ document.write( addy59958 );
+ document.write( '<\/a>' );
+ //-->
+ </script><script language='JavaScript' type='text/javascript'>
+ <!--
+ document.write( '<span style=\'display: none;\'>' );
+ //-->
+ </script>Este e-mail está protegido contra robots de spam, necesita habilitar el Javascript de su navegador para poder verlo.
+ <script language='JavaScript' type='text/javascript'>
+ <!--
+ document.write( '</' );
+ document.write( 'span>' );
+ //-->
+ </script>	</td>
+</tr>
+<tr>
+	<td width="40" >
+		<img src="/images/M_images/con_tel.png" alt="Teléfono: "  />	</td>
+	<td>
+		4371-5386	</td>
+</tr>
+<tr>
+	<td width="40" >
+	</td>
+	<td>
+		<a href="http://www.asyaconsultores.com.ar" target="_blank">
+			http://www.asyaconsultores.com.ar</a>
+	</td>
+</tr>
+</table>
+<br />
+			</td>
+		</tr>
+		</table>
+	</td>
+	<td>&nbsp;</td>
+</tr>
+<tr>
+	<td></td>
+</tr>
+<tr>
+	<td colspan="2">
+	<br /><br />
+	<form action="/contacto" method="post" name="emailForm" id="emailForm" class="form-validate">
+		<div class="contact_email">
+			<label for="contact_name">
+				&nbsp;Escriba su nombre y apellidos:
+			</label>
+			<br />
+			<input type="text" name="name" id="contact_name" size="30" class="inputbox" value="" />
+			<br />
+			<label id="contact_emailmsg" for="contact_email">
+				&nbsp;Dirección de e-mail:
+			</label>
+			<br />
+			<input type="text" id="contact_email" name="email" size="30" value="" class="inputbox required validate-email" maxlength="100" />
+			<br />
+			<label for="contact_subject">
+				&nbsp;Tema del mensaje:
+			</label>
+			<br />
+			<input type="text" name="subject" id="contact_subject" size="30" class="inputbox" value="" />
+			<br /><br />
+			<label id="contact_textmsg" for="contact_text">
+				&nbsp;Escriba su mensaje:
+			</label>
+			<br />
+			<textarea cols="50" rows="10" name="text" id="contact_text" class="inputbox required"></textarea>
+						<br />
+				<input type="checkbox" name="email_copy" id="contact_email_copy" value="1"  />
+				<label for="contact_email_copy">
+					Enviar una copia de este mensaje a su propio E-mail				</label>
+						<br />
+			<br />
+			<button class="button validate" type="submit">Enviar</button>
+		</div>
+
+	<input type="hidden" name="option" value="com_contact" />
+	<input type="hidden" name="view" value="contact" />
+	<input type="hidden" name="id" value="1" />
+	<input type="hidden" name="task" value="submit" />
+	<input type="hidden" name="89239465ae7b91915c3193c02f3de926" value="1" />	</form>
+	<br />
+	</td>
+</tr>
+</table>
+</div>
+";s:4:"head";a:10:{s:5:"title";s:13:"ASyA Contacto";s:11:"description";s:24:"ASyA Asociados Direccion";s:4:"link";s:0:"";s:8:"metaTags";a:2:{s:10:"http-equiv";a:1:{s:12:"content-type";s:24:"text/html; charset=utf-8";}s:8:"standard";a:2:{s:6:"robots";s:13:"index, follow";s:8:"keywords";s:23:"Contadores, Consultores";}}s:5:"links";a:0:{}s:11:"styleSheets";a:0:{}s:5:"style";a:0:{}s:7:"scripts";a:2:{s:28:"/media/system/js/mootools.js";s:15:"text/javascript";s:28:"/media/system/js/validate.js";s:15:"text/javascript";}s:6:"script";a:1:{s:15:"text/javascript";s:415:"<!--
+		function validateForm( frm ) {
+			var valid = document.formvalidator.isValid(frm);
+			if (valid == false) {
+				// do field validation
+				if (frm.email.invalid) {
+					alert( "Introduzca una dirección de e-mail correcta." );
+				} else if (frm.text.invalid) {
+					alert( "Asegúrese de que el formulario esta completo y correcto." );
+				}
+				return false;
+			} else {
+				frm.submit();
+			}
+		}
+		// -->";}s:6:"custom";a:0:{}}s:7:"pathway";a:1:{i:0;O:8:"stdClass":2:{s:4:"name";s:8:"Contacto";s:4:"link";s:20:"index.php?Itemid=129";}}s:6:"module";a:0:{}}
---
+++ backup/config.php
@@ -0,0 +1,12 @@
+<?php
+// Archivo config.php 
+define('SMTP_HOST', 'r6000494.ferozo.com');
+define('SMTP_USER', 'asya@asyaconsultores.com.ar');
+define('SMTP_PASSWORD', 'Lavalle1546');
+define('SMTP_PORT', 465);
+
+// Recaptcha
+define('RECAPTCHA_SITE_KEY', '6LfYM-0qAAAAACI1C1lG57UFuqWUht4LxSs37LBN');
+define('RECAPTCHA_SECRET_KEY', '6LfYM-0qAAAAAIA66ppfs6NeXzXt4ltNQEMpkGJe');
+
+?>
